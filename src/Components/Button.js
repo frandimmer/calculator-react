bea7ca8fbc8438b1ea316936aec440ce305d133e
Francisco Dimmer <ftdimmer@gmail.com>
2022-07-21 17:00:15 -0300

    simple calculator made using react

---
+++ src/Components/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../Styles/Button.css';
+
+function Button(props) {
+
+  const isOperator = value => {
+    return isNaN(value) && (value != '.') && (value != '=');
+  };
+
+  return (
+    <div
+    className={`button-container ${isOperator(props.children) ? 'operator' : null}`}
+    onClick={() => props.handleClick(props.children)}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Button;
---
+++ src/Components/ClearButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/ClearButton.css'
+
+const ClearButton = (props) => (
+    <div 
+    className='clear-button'
+    onClick={props.handleClear}
+    >
+        {props.children}
+    </div>
+)
+
+export default ClearButton;
